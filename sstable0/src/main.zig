5c6a75edd633da5aa59a7c57003f80a3f1c01445
mtso <~@mtso.io>
2022-05-23 14:34:27 -0700

    Port rb tree to latest zig compat

--- sstable0/src/main.zig
+++ sstable0/src/main.zig
@@ -1,33 +1,36 @@
 const std = @import("std");
+const rb = @import("rb.zig");
 
-const PriorityDequeue = std.PriorityDequeue;
+const Number = struct {
+    const Self = @This();
 
-const Entry = struct {
-    key: u128,
-    value: u128,
+    node: rb.Node,
+    value: u64,
+
+    pub fn from(node: *rb.Node) *Self {
+        return @fieldParentPtr(Self, "node", node);
+    }
+
+    pub fn order(l: *rb.Node, r: *rb.Node) std.math.Order {
+        const left = Number.from(l);
+        const right = Number.from(r);
+        return std.math.order(left.value, right.value);
+    }
 };
 
-fn lessThanComparison(context: void, a: Entry, b: Entry) std.math.Order {
-    _ = context;
-    return std.math.order(a.key, b.key);
-}
-
-const NumDQ = PriorityDequeue(Entry, void, lessThanComparison);
-
 pub fn main() anyerror!void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    var q = NumDQ.init(allocator, {});
-    defer q.deinit();
+    var tree: rb.Tree = undefined;
+    tree.init(Number.order);
 
-    try q.add(Entry{ .key = 1, .value = 1 });
-    try q.add(Entry{ .key = 3, .value = 3 });
-    try q.add(Entry{ .key = 2, .value = 2 });
+    var n = Number{
+        .node = undefined,
+        .value = 1,
+    };
 
-    var iter = q.iterator();
-    while (iter.next()) |e| {
-        std.debug.print("k:{d} v:{d}\n", .{
-            e.key, e.value,
-        });
+    _ = tree.insert(&n.node);
+
+    if (tree.first()) |node| {
+        const value = Number.from(node).value;
+        std.debug.print("first: {d}\n", .{ value });
     }
 }
