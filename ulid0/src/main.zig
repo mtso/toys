123f5eb70319d3d3c6f88a48bec53c9e39075816
mtso <~@mtso.io>
2022-05-26 09:43:59 -0700

    Make ULID default parsing safer

--- ulid0/src/main.zig
+++ ulid0/src/main.zig
@@ -4,14 +4,14 @@ const math = std.math;
 const time = std.time;
 const mem = std.mem;
 
-const base32crockford = [_]u8{
+const base32Crockford = [_]u8{
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K',
     'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X',
     'Y', 'Z',
 };
 
-const base32crockfordreversed = [_]u8{
+const base32CrockfordInv = [_]u8{
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -26,6 +26,22 @@ const base32crockfordreversed = [_]u8{
     21, 0,  22, 23, 24, 25, 26, 0,  27, 28,
     29, 30, 31, 0,  0,  0,  0,  0,  0,  0,
     0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+};
+
+const base32CrockfordValid = [128]bool{
+    false, false, false, false, false, false, false, false, false, false,
+    false, false, false, false, false, false, false, false, false, false,
+    false, false, false, false, false, false, false, false, false, false,
+    false, false, false, false, false, false, false, false, false, false,
+    false, false, false, false, false, false, false, false, true,  true, // 48 0 digits
+    true,  true,  true,  true,  true,  true,  true,  true,  false, false,
+    false, false, false, false, false, true,  true,  true, true, true, // 65 A uppercase
+    true,  true,  true,  false, true,  true,  false, true, true, false,
+    true,  true,  true,  true,  true,  false, true,  true, true, true,
+    true, false, false, false, false, false, false, true,  true,  true, // 97 a lowercase
+    true, true,  true,  true,  true,  false, true,  true,  false, true,
+    true, false, true,  true,  true,  true,  true,  false, true,  true,
+    true, true,  true,  false, false, false, false, false,
 };
 
 pub fn order(a: Ulid, b: Ulid) math.Order {
@@ -78,32 +94,32 @@ pub const Ulid = packed struct {
     }
 
     pub fn fillBase32(self: Self, bytes: *[26]u8) void {
-        bytes[0] = base32crockford[self.bytes[0] >> 5 & 0x1F];
-        bytes[1] = base32crockford[self.bytes[0] & 0x1F];
-        bytes[2] = base32crockford[self.bytes[1] >> 3];
-        bytes[3] = base32crockford[self.bytes[1] << 2 & 0x1F | self.bytes[2] >> 6];
-        bytes[4] = base32crockford[self.bytes[2] >> 1 & 0x1F];
-        bytes[5] = base32crockford[self.bytes[2] << 4 & 0x1F | self.bytes[3] >> 4];
-        bytes[6] = base32crockford[self.bytes[3] << 1 & 0x1F | self.bytes[4] >> 7];
-        bytes[7] = base32crockford[self.bytes[4] >> 2 & 0x1F];
-        bytes[8] = base32crockford[self.bytes[4] << 3 & 0x1F | self.bytes[5] >> 5];
-        bytes[9] = base32crockford[self.bytes[5] & 0x1F];
-        bytes[10] = base32crockford[self.bytes[6] >> 3];
-        bytes[11] = base32crockford[self.bytes[6] << 2 & 0x1F | self.bytes[7] >> 6];
-        bytes[12] = base32crockford[self.bytes[7] >> 1 & 0x1F];
-        bytes[13] = base32crockford[self.bytes[7] << 4 & 0x1F | self.bytes[8] >> 4];
-        bytes[14] = base32crockford[self.bytes[8] << 1 & 0x1F | self.bytes[9] >> 7];
-        bytes[15] = base32crockford[self.bytes[9] >> 2 & 0x1F];
-        bytes[16] = base32crockford[self.bytes[9] << 3 & 0x1F | self.bytes[10] >> 5];
-        bytes[17] = base32crockford[self.bytes[10] & 0x1F];
-        bytes[18] = base32crockford[self.bytes[11] >> 3];
-        bytes[19] = base32crockford[self.bytes[11] << 2 & 0x1F | self.bytes[12] >> 6];
-        bytes[20] = base32crockford[self.bytes[12] >> 1 & 0x1F];
-        bytes[21] = base32crockford[self.bytes[12] << 4 & 0x1F | self.bytes[13] >> 4];
-        bytes[22] = base32crockford[self.bytes[13] << 1 & 0x1F | self.bytes[14] >> 7];
-        bytes[23] = base32crockford[self.bytes[14] >> 2 & 0x1F];
-        bytes[24] = base32crockford[self.bytes[14] << 3 & 0x1F | self.bytes[15] >> 5];
-        bytes[25] = base32crockford[self.bytes[15] & 0x1F];
+        bytes[0] = base32Crockford[self.bytes[0] >> 5 & 0x1F];
+        bytes[1] = base32Crockford[self.bytes[0] & 0x1F];
+        bytes[2] = base32Crockford[self.bytes[1] >> 3];
+        bytes[3] = base32Crockford[self.bytes[1] << 2 & 0x1F | self.bytes[2] >> 6];
+        bytes[4] = base32Crockford[self.bytes[2] >> 1 & 0x1F];
+        bytes[5] = base32Crockford[self.bytes[2] << 4 & 0x1F | self.bytes[3] >> 4];
+        bytes[6] = base32Crockford[self.bytes[3] << 1 & 0x1F | self.bytes[4] >> 7];
+        bytes[7] = base32Crockford[self.bytes[4] >> 2 & 0x1F];
+        bytes[8] = base32Crockford[self.bytes[4] << 3 & 0x1F | self.bytes[5] >> 5];
+        bytes[9] = base32Crockford[self.bytes[5] & 0x1F];
+        bytes[10] = base32Crockford[self.bytes[6] >> 3];
+        bytes[11] = base32Crockford[self.bytes[6] << 2 & 0x1F | self.bytes[7] >> 6];
+        bytes[12] = base32Crockford[self.bytes[7] >> 1 & 0x1F];
+        bytes[13] = base32Crockford[self.bytes[7] << 4 & 0x1F | self.bytes[8] >> 4];
+        bytes[14] = base32Crockford[self.bytes[8] << 1 & 0x1F | self.bytes[9] >> 7];
+        bytes[15] = base32Crockford[self.bytes[9] >> 2 & 0x1F];
+        bytes[16] = base32Crockford[self.bytes[9] << 3 & 0x1F | self.bytes[10] >> 5];
+        bytes[17] = base32Crockford[self.bytes[10] & 0x1F];
+        bytes[18] = base32Crockford[self.bytes[11] >> 3];
+        bytes[19] = base32Crockford[self.bytes[11] << 2 & 0x1F | self.bytes[12] >> 6];
+        bytes[20] = base32Crockford[self.bytes[12] >> 1 & 0x1F];
+        bytes[21] = base32Crockford[self.bytes[12] << 4 & 0x1F | self.bytes[13] >> 4];
+        bytes[22] = base32Crockford[self.bytes[13] << 1 & 0x1F | self.bytes[14] >> 7];
+        bytes[23] = base32Crockford[self.bytes[14] >> 2 & 0x1F];
+        bytes[24] = base32Crockford[self.bytes[14] << 3 & 0x1F | self.bytes[15] >> 5];
+        bytes[25] = base32Crockford[self.bytes[15] & 0x1F];
     }
 
     pub fn format(value: Self, comptime fmt: []const u8, options: std.fmt.FormatOptions, writer: anytype) !void {
@@ -144,37 +160,95 @@ pub const Ulid = packed struct {
     pub const ParseError = error{
         StringTooShort,
         WouldOverflowTimestamp,
+        InvalidChar,
     };
 
     pub fn parseBase32(bytes: []const u8) ParseError!Self {
         if (bytes.len < 26) return error.StringTooShort;
         if (bytes[0] > '7') return error.WouldOverflowTimestamp;
+        for (bytes[0..26]) |byte| try validateByte(byte);
         return Self{
             .bytes = [16]u8{
-                base32crockfordreversed[bytes[0]] << 5 | base32crockfordreversed[bytes[1]],
-                base32crockfordreversed[bytes[2]] << 3 | base32crockfordreversed[bytes[3]] >> 2,
-                base32crockfordreversed[bytes[3]] << 6 | base32crockfordreversed[bytes[4]] << 1 | base32crockfordreversed[bytes[5]] >> 4,
-                base32crockfordreversed[bytes[5]] << 4 | base32crockfordreversed[bytes[6]] >> 1,
-                base32crockfordreversed[bytes[6]] << 7 | base32crockfordreversed[bytes[7]] << 2 | base32crockfordreversed[bytes[8]] >> 3,
-                base32crockfordreversed[bytes[8]] << 5 | base32crockfordreversed[bytes[9]],
-                base32crockfordreversed[bytes[10]] << 3 | base32crockfordreversed[bytes[11]] >> 2,
-                base32crockfordreversed[bytes[11]] << 6 | base32crockfordreversed[bytes[12]] << 1 | base32crockfordreversed[bytes[13]] >> 4,
-                base32crockfordreversed[bytes[13]] << 4 | base32crockfordreversed[bytes[14]] >> 1,
-                base32crockfordreversed[bytes[14]] << 7 | base32crockfordreversed[bytes[15]] << 2 | base32crockfordreversed[bytes[16]] >> 3,
-                base32crockfordreversed[bytes[16]] << 5 | base32crockfordreversed[bytes[17]],
-                base32crockfordreversed[bytes[18]] << 3 | base32crockfordreversed[bytes[19]] >> 2,
-                base32crockfordreversed[bytes[19]] << 6 | base32crockfordreversed[bytes[20]] << 1 | base32crockfordreversed[bytes[21]] >> 4,
-                base32crockfordreversed[bytes[21]] << 4 | base32crockfordreversed[bytes[22]] >> 1,
-                base32crockfordreversed[bytes[22]] << 7 | base32crockfordreversed[bytes[23]] << 2 | base32crockfordreversed[bytes[24]] >> 3,
-                base32crockfordreversed[bytes[24]] << 5 | base32crockfordreversed[bytes[25]],
+                base32CrockfordInv[bytes[0]] << 5 | base32CrockfordInv[bytes[1]],
+                base32CrockfordInv[bytes[2]] << 3 | base32CrockfordInv[bytes[3]] >> 2,
+                base32CrockfordInv[bytes[3]] << 6 | base32CrockfordInv[bytes[4]] << 1 | base32CrockfordInv[bytes[5]] >> 4,
+                base32CrockfordInv[bytes[5]] << 4 | base32CrockfordInv[bytes[6]] >> 1,
+                base32CrockfordInv[bytes[6]] << 7 | base32CrockfordInv[bytes[7]] << 2 | base32CrockfordInv[bytes[8]] >> 3,
+                base32CrockfordInv[bytes[8]] << 5 | base32CrockfordInv[bytes[9]],
+                base32CrockfordInv[bytes[10]] << 3 | base32CrockfordInv[bytes[11]] >> 2,
+                base32CrockfordInv[bytes[11]] << 6 | base32CrockfordInv[bytes[12]] << 1 | base32CrockfordInv[bytes[13]] >> 4,
+                base32CrockfordInv[bytes[13]] << 4 | base32CrockfordInv[bytes[14]] >> 1,
+                base32CrockfordInv[bytes[14]] << 7 | base32CrockfordInv[bytes[15]] << 2 | base32CrockfordInv[bytes[16]] >> 3,
+                base32CrockfordInv[bytes[16]] << 5 | base32CrockfordInv[bytes[17]],
+                base32CrockfordInv[bytes[18]] << 3 | base32CrockfordInv[bytes[19]] >> 2,
+                base32CrockfordInv[bytes[19]] << 6 | base32CrockfordInv[bytes[20]] << 1 | base32CrockfordInv[bytes[21]] >> 4,
+                base32CrockfordInv[bytes[21]] << 4 | base32CrockfordInv[bytes[22]] >> 1,
+                base32CrockfordInv[bytes[22]] << 7 | base32CrockfordInv[bytes[23]] << 2 | base32CrockfordInv[bytes[24]] >> 3,
+                base32CrockfordInv[bytes[24]] << 5 | base32CrockfordInv[bytes[25]],
+            },
+        };
+    }
+
+    fn validateByte(byte: u8) ParseError!void {
+        if (byte >= 128 or !base32CrockfordValid[byte]) {
+            return error.InvalidChar;
+        }
+    }
+
+    pub fn parseBase32Unsafe(bytes: []const u8) ParseError!Self {
+        if (bytes.len < 26) return error.StringTooShort;
+        if (bytes[0] > '7') return error.WouldOverflowTimestamp;
+        return Self{
+            .bytes = [16]u8{
+                base32CrockfordInv[bytes[0]] << 5 | base32CrockfordInv[bytes[1]],
+                base32CrockfordInv[bytes[2]] << 3 | base32CrockfordInv[bytes[3]] >> 2,
+                base32CrockfordInv[bytes[3]] << 6 | base32CrockfordInv[bytes[4]] << 1 | base32CrockfordInv[bytes[5]] >> 4,
+                base32CrockfordInv[bytes[5]] << 4 | base32CrockfordInv[bytes[6]] >> 1,
+                base32CrockfordInv[bytes[6]] << 7 | base32CrockfordInv[bytes[7]] << 2 | base32CrockfordInv[bytes[8]] >> 3,
+                base32CrockfordInv[bytes[8]] << 5 | base32CrockfordInv[bytes[9]],
+                base32CrockfordInv[bytes[10]] << 3 | base32CrockfordInv[bytes[11]] >> 2,
+                base32CrockfordInv[bytes[11]] << 6 | base32CrockfordInv[bytes[12]] << 1 | base32CrockfordInv[bytes[13]] >> 4,
+                base32CrockfordInv[bytes[13]] << 4 | base32CrockfordInv[bytes[14]] >> 1,
+                base32CrockfordInv[bytes[14]] << 7 | base32CrockfordInv[bytes[15]] << 2 | base32CrockfordInv[bytes[16]] >> 3,
+                base32CrockfordInv[bytes[16]] << 5 | base32CrockfordInv[bytes[17]],
+                base32CrockfordInv[bytes[18]] << 3 | base32CrockfordInv[bytes[19]] >> 2,
+                base32CrockfordInv[bytes[19]] << 6 | base32CrockfordInv[bytes[20]] << 1 | base32CrockfordInv[bytes[21]] >> 4,
+                base32CrockfordInv[bytes[21]] << 4 | base32CrockfordInv[bytes[22]] >> 1,
+                base32CrockfordInv[bytes[22]] << 7 | base32CrockfordInv[bytes[23]] << 2 | base32CrockfordInv[bytes[24]] >> 3,
+                base32CrockfordInv[bytes[24]] << 5 | base32CrockfordInv[bytes[25]],
             },
         };
     }
 };
 
-test "parseBase32" {
+test "parseBase32 max" {
+    const strings = [_][]const u8{
+        "7zzzzzzzzzzzzzzzzzzzzzzzzz",
+        "7ZZZZZZZZZZZZZZZZZZZZZZZZZ",
+    };
+
+    for (strings) |input| {
+        const ulid = try Ulid.parseBase32(input);
+        const expected: [16]u8 = [_]u8{0xFF} ** 16;
+        try std.testing.expect(std.mem.eql(u8, expected[0..], ulid.bytes[0..]));
+    }
+}
+
+test "parseBase32 error.InvalidChar" {
+    const strings = [_][]const u8{
+        "01g3wb8czjnh0000000000ilou",
+        "01G3WB8CZJNH0000000000ILOU",
+    };
+
+    for (strings) |input| {
+        const ulid = Ulid.parseBase32(input);
+        try std.testing.expectError(error.InvalidChar, ulid);
+    }
+}
+
+test "parseBase32Unsafe" {
     const input = "7ZZZZZZZZZZZZZZZZZZZZZZZZZ";
-    const ulid = try Ulid.parseBase32(input);
+    const ulid = try Ulid.parseBase32Unsafe(input);
     const expected: [16]u8 = [_]u8{0xFF} ** 16;
     try std.testing.expect(std.mem.eql(u8, expected[0..], ulid.bytes[0..]));
 }
